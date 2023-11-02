d16b444673f4a49f9b4499559afdfc4a28315a25
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-11-02 22:05:23 +0700

    Update examples for MhaFlnn models

---
+++ examples/exam_mha_flnn_multi_class_classification.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 21:35, 02/11/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from reflame import Data, FlnnClassifier
+from sklearn.datasets import load_iris
+
+
+## Load data object
+X, y = load_iris(return_X_y=True)
+data = Data(X, y)
+
+## Split train and test
+data.split_train_test(test_size=0.2, random_state=2, inplace=True, shuffle=True)
+print(data.X_train.shape, data.X_test.shape)
+
+## Scaling dataset
+data.X_train, scaler_X = data.scale(data.X_train, scaling_methods=("standard", "minmax"))
+data.X_test = scaler_X.transform(data.X_test)
+
+data.y_train, scaler_y = data.encode_label(data.y_train)
+data.y_test = scaler_y.transform(data.y_test)
+
+## Create model
+model = FlnnClassifier(expand_name="chebyshev", n_funcs=4, act_name="elu",
+                      obj_name="CEL", max_epochs=100, batch_size=32, optimizer="SGD", verbose=True)
+
+## Train the model
+model.fit(X=data.X_train, y=data.y_train)
+
+## Test the model
+y_pred = model.predict(data.X_test, return_prob=True)
+print(y_pred)
+
+## Calculate some metrics
+print(model.score(X=data.X_test, y=data.y_test, method="AS"))
+print(model.scores(X=data.X_test, y=data.y_test, list_methods=["PS", "RS", "NPV", "F1S", "F2S"]))
+print(model.evaluate(y_true=data.y_test, y_pred=y_pred, list_metrics=["F2S", "CKS", "FBS"]))
---
+++ examples/exam_mha_flnn_regression.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 21:35, 02/11/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+from reflame import Data, MhaFlnnRegressor
+from sklearn.datasets import load_diabetes
+
+
+## Load data object
+X, y = load_diabetes(return_X_y=True)
+data = Data(X, y)
+
+## Split train and test
+data.split_train_test(test_size=0.2, random_state=2)
+print(data.X_train.shape, data.X_test.shape)
+
+## Scaling dataset
+data.X_train, scaler_X = data.scale(data.X_train, scaling_methods=("minmax", ))
+data.X_test = scaler_X.transform(data.X_test)
+
+data.y_train, scaler_y = data.scale(data.y_train, scaling_methods=("standard", ))
+data.y_test = scaler_y.transform(np.reshape(data.y_test, (-1, 1)))
+
+## Create model
+opt_paras = {"name": "GA", "epoch": 250, "pop_size": 30}
+model = MhaFlnnRegressor(expand_name="chebyshev", n_funcs=3, act_name="elu", obj_name="RMSE", optimizer="BaseGA", optimizer_paras=opt_paras, verbose=True)
+
+## Train the model
+model.fit(data.X_train, data.y_train)
+
+## Test the model
+y_pred = model.predict(data.X_test)
+print(y_pred)
+
+## Calculate some metrics
+print(model.score(X=data.X_test, y=data.y_test, method="RMSE"))
+print(model.scores(X=data.X_test, y=data.y_test, list_methods=["R2", "NSE", "MAPE"]))
+print(model.evaluate(y_true=data.y_test, y_pred=y_pred, list_metrics=["R2", "NSE", "MAPE", "NNSE"]))
